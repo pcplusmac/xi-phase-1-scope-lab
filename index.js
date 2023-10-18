97e9e940543a910bce4439b8cdd8a94aeece174b
el-sam <samurai.h@yahoo.com>
2023-10-18 13:03:16 +0800

    change the case of customer name via a funtion

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // Write your solution in this file!
 // Declare customerName
 customerName = "bob";
+
+// Define function to modify varible cutomerName
+function upperCaseCustomerName() {
+    customerName = "BOB";
+}
