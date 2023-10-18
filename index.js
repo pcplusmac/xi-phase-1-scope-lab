599947b704b487154f0056284b115a77ed009321
el-sam <samurai.h@yahoo.com>
2023-10-18 13:09:48 +0800

    add 'var' keyword infront of 'customerName'; declare global scope variable 'bestCustomer' and assign a value

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // Write your solution in this file!
 // Declare customerName
-customerName = "bob";
+var customerName = "bob";
 
 // Define function to modify varible cutomerName
 function upperCaseCustomerName() {
     customerName = "BOB";
+}
+
+//  Define function to set the best customer
+function setBestCustomer() {
+    bestCustomer = "not bob";
+
 }
