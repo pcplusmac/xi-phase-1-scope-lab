a28b38b6513e7700b233b3ba75f1aad191193629
el-sam <samurai.h@yahoo.com>
2025-09-14 14:08:45 +0800

    all test done

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 // Write your solution in this file!
 // Declare customerName
 var customerName = "bob";
-leastFavoriteCustomer = "ali";
+const leastFavoriteCustomer = "ali";
 
 // Define function to modify varible cutomerName
 function upperCaseCustomerName() {
     customerName = "BOB";
+    // console.log(customerName.toUpperCase());
+    // let name = customerName.toUpperCase();
+    // return name;
+
 }
 
 //  Define function to set the best customer
 function setBestCustomer() {
+    // without using either 'let' or 'const' for the 
+    // variable, it will be global scope 
     bestCustomer = "not bob";
+    
 
 }
 
@@ -20,6 +27,12 @@ function overwriteBestCustomer() {
     console.log(leastFavoriteCustomer);
 
 }
-// overwriteBestCustomer()
-// changeLeastFavoriteCustomer();
-// overwriteBestCustomer()
+
+function changeLeastFavoriteCustomer() {
+    // code below is trying to change the value of the const variable
+    // it will have error, check it by comment out the console.log line
+    // you can only change it after change 'const' to 'let'. try it out 
+    
+    leastFavoriteCustomer = "changed you";
+    // console.log(leastFavoriteCustomer);
+}
